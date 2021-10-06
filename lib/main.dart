0879a2a722df25c50e184cda7cd617a4f2e1a0e5
therokibul <therokibul@gmail.com>
2021-10-06 09:29:45 +0600

    day12 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,46 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green.shade800,
-          centerTitle: true,
-          leading: Icon(Icons.trending_up_outlined),
-          title: Text(
-            'PROFILE',
-            style: TextStyle(
-              fontSize: 40.0,
-              color: Colors.black,
-            ),
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.flutter_dash_outlined,
-                size: 200.0,
-              ),
-              Text(
-                'Rokibul Hasan',
-                style: TextStyle(fontSize: 40.0),
-              ),
-              Text(
-                '@therokibul',
-                style: TextStyle(fontSize: 30.0),
-              ),
-            ],
-          ),
-        ),
+        body: HomePage(),
       ),
-    ),
-  );
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.vertical,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('back'),
+            Text('Content'),
+            Text('Filter'),
+          ],
+        ),
+        Image.network('https://images.unsplash.com/photo-1632685166377-bf74f440457f?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+      ],
+    );
+  }
 }
