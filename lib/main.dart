7fc0abe587cf88395a96d0d39c30be86c733f3c1
therokibul <therokibul@gmail.com>
2021-10-06 12:08:33 +0600

    day15_B completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -14,7 +10,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.grey,
+        backgroundColor: Colors.black,
+        // appBar: AppBar(
+        //   backgroundColor: Colors.white,
+        //   title: Text(
+        //     'Layout',
+        //     style: TextStyle(color: Colors.black),
+        //   ),
+        // ),
         body: HomePage(),
       ),
     );
@@ -31,24 +34,140 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Stack(
-        children: [
-          Container(
-            height: 200,
-            width: 200,
-            color: Colors.blue,
+    return ListView(
+      children: [
+        Stack(
+          clipBehavior: Clip.none,
+          alignment: Alignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.only(bottom: 180),
+              color: Colors.green,
+              height: 280,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    'Back',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  Text(
+                    'Content',
+                    style: TextStyle(fontSize: 40, color: Colors.white),
+                  ),
+                  Text(
+                    'Filter',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: 180,
+              child: CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('images/lash.jpg'),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 110,
+        ),
+        Text(
+          'Victoria Robertson',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white, fontSize: 40),
+        ),
+        Text(
+          'Flutter Developer',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          padding: EdgeInsets.fromLTRB(50, 10, 0, 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: Colors.white,
           ),
-          Container(
-            height: 180,
-            width: 180,
-            color: Colors.deepPurple,
+          child: Text(
+            'Posts',
+            textAlign: TextAlign.start,
+            style: TextStyle(color: Colors.green, fontSize: 18),
           ),
-          Positioned(
-            top: 200,
-            child: )
-        ],
-      ),
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.flutter_dash_outlined,
+            color: Colors.white,
+          ),
+          title: Text(
+            'Posts',
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          subtitle: Text(
+            'Hell want to use your yacht, and I dont want this thing smelling like fish.',
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Divider(
+           color: Colors.white,
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.flutter_dash_outlined,
+            color: Colors.white,
+          ),
+          title: Text(
+            'Posts',
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          subtitle: Text(
+            'Hell want to use your yacht, and I dont want this thing smelling like fish.',
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Divider(
+          
+          color: Colors.white,
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.flutter_dash_outlined,
+            color: Colors.white,
+          ),
+          title: Text(
+            'Posts',
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          subtitle: Text(
+            'Hell want to use your yacht, and I dont want this thing smelling like fish.',
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Divider(
+          color: Colors.white,
+        ),
+      ],
     );
   }
 }
