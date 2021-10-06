b26c36fe83003a72ef518eadf6812bdba355e00c
therokibul <therokibul@gmail.com>
2021-10-06 11:20:44 +0600

    title

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,14 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey,
-        body: HomePage(),
+        appBar: AppBar(
+          leading: Icon(Icons.mail_outlined),
+          title: Text('Hello World'),
+        ),
       ),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Stack(
-        children: [
-          Container(
-            height: 200,
-            width: 200,
-            color: Colors.blue,
-          ),
-          Container(
-            height: 180,
-            width: 180,
-            color: Colors.deepPurple,
-          ),
-          Positioned(
-            top: 200,
-            child: )
-        ],
-      ),
-    );
-  }
+    ),
+  );
 }
