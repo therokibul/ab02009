ebd7b095aeae3a29a9bfed2d486ad7be270348d8
therokibul <therokibul@gmail.com>
2021-10-11 10:34:29 +0600

    day15_A

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,21 @@ void main() {
           leading: Icon(Icons.mail_outlined),
           title: Text('Hello World'),
         ),
+        body: Center(
+          child: Column(
+            children: [
+              Icon(
+                Icons.visibility_outlined,
+                size: 250,
+                color: Colors.teal.shade200,
+              ),
+              Text(
+                'Hello World',
+                style: TextStyle(fontSize: 40, color: Colors.red),
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
