96841733ef09f075983420e99fec0c59e825a87e
therokibul <therokibul@gmail.com>
2021-10-23 11:19:21 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,31 @@ void main() {
         body: Center(
           child: Column(
             children: [
-              Icon(
-                Icons.visibility_outlined,
-                size: 250,
-                color: Colors.teal.shade200,
+              Image(
+                image: NetworkImage(
+                    'https://images.unsplash.com/photo-1634901705540-51ecd093b267?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=388&q=80'),
+                height: 200,
+              ),
+              Image(
+                image: AssetImage('images/wrdo.png'),
+                height: 200,
               ),
               Text(
                 'Hello World',
                 style: TextStyle(fontSize: 40, color: Colors.red),
+              ),
+              Container(
+                width: 300,
+                height: 200,
+                color: Colors.teal,
+                child: Text(
+                  '23/10/2021',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ],
           ),
