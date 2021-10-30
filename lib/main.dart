d7a3e68996b7e1b2f16504a1c8617e70ca1cd46c
therokibul <therokibul@gmail.com>
2021-10-30 12:39:01 +0600

    30-10

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.purple,
-          centerTitle: true,
-          leading: Icon(Icons.mail_outlined),
-          title: Text('Hello World'),
-        ),
-        body: Center(
-          child: Column(
+          backgroundColor: Colors.black,
+          body: ListView(
             children: [
-              Image(
-                image: NetworkImage(
-                    'https://images.unsplash.com/photo-1634901705540-51ecd093b267?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=388&q=80'),
-                height: 200,
-              ),
-              Image(
-                image: AssetImage('images/wrdo.png'),
-                height: 200,
-              ),
-              Text(
-                'Hello World',
-                style: TextStyle(fontSize: 40, color: Colors.red),
-              ),
-              Container(
-                width: 300,
-                height: 200,
-                color: Colors.teal,
-                child: Text(
-                  '23/10/2021',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.white,
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Container(
+                    height: 300,
+                    color: Colors.green,
                   ),
-                ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        'Settings',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'Profile',
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                      Text(
+                        'Settings',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    top: 210,
+                    left: 180,
+                    child: CircleAvatar(
+                      radius: 80,
+                      backgroundColor: Colors.white,
+                      child: CircleAvatar(
+                        radius: 70,
+                        backgroundImage: AssetImage('images/cat.jpg'),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
-          ),
-        ),
-      ),
-    ),
-  );
+          )),
+    );
+  }
 }
