f94ade13b2e3a4705f85645f66e68389b96022ba
therokibul <therokibul@gmail.com>
2021-11-24 11:36:45 +0600

    24-11

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,75 @@
-import 'package:ab02009/screens/home_mobile.dart';
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
-import 'screens/homepage.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        fontFamily: 'YujiMai',
-      ),
-      home: Scaffold(
-        body: Layout(),
-      ),
+      home: AnimationScreen(),
     );
   }
 }
 
-class Layout extends StatelessWidget {
-  const Layout({Key? key}) : super(key: key);
+class AnimationScreen extends StatefulWidget {
+  const AnimationScreen({Key? key}) : super(key: key);
+
+  @override
+  _AnimationScreenState createState() => _AnimationScreenState();
+}
+
+class _AnimationScreenState extends State<AnimationScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> curveAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController = AnimationController(
+      duration: Duration(seconds: 4),
+      vsync: this,
+    );
+    _animationController.repeat();
+    curveAnimation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.elasticInOut,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-      if (constraints.maxWidth < 600) {
-        return HomeMobile();
-      } else { 
-        return HomePage();
-      }
-    });
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            RotationTransition(
+              child: Container(
+                width: 300,
+                height: 300,
+                color: Colors.green,
+              ),
+              turns: Tween<double>(begin: 0, end: 1).animate(curveAnimation),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_animationController.isAnimating) {
+            _animationController.stop();
+          } 
+          else {
+            _animationController.repeat();
+          }
+        },
+      ),
+    );
   }
 }
