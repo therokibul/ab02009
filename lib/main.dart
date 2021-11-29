151d55fc8e8b86564267812af83e9c7ff8ae166c
therokibul <therokibul@gmail.com>
2021-11-29 13:05:46 +0600

    ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,65 +8,127 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AnimationScreen(),
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: Calculator(),
+      
     );
   }
 }
 
-class AnimationScreen extends StatefulWidget {
-  const AnimationScreen({Key? key}) : super(key: key);
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
 
   @override
-  _AnimationScreenState createState() => _AnimationScreenState();
+  _CalculatorState createState() => _CalculatorState();
 }
 
-class _AnimationScreenState extends State<AnimationScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> curveAnimation;
+class _CalculatorState extends State<Calculator> {
+  String equation = '0';
+  String result = '0';
+  String expresstion = '';
+  double equationFontSize = 40;
+  double resultFontSize = 50;
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _animationController = AnimationController(
-      duration: Duration(seconds: 4),
-      vsync: this,
-    );
-    _animationController.repeat();
-    curveAnimation = CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.elasticInOut,
+  Widget buidButton(String buttonText, double buttonHeight, Color buttonColor) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+          color: buttonColor, borderRadius: BorderRadius.circular(24)),
+      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+      child: MaterialButton(
+        onPressed: () {},
+        padding: EdgeInsets.all(16),
+        child: Text(
+          buttonText,
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            RotationTransition(
-              child: Container(
-                width: 300,
-                height: 300,
-                color: Colors.green,
-              ),
-              turns: Tween<double>(begin: 0, end: 1).animate(curveAnimation),
-            ),
-          ],
-        ),
+      backgroundColor: Color(0xff17171C),
+      appBar: AppBar(
+        title: Text('Calculator'),
+        backgroundColor: Color(0xff17171C),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (_animationController.isAnimating) {
-            _animationController.stop();
-          } 
-          else {
-            _animationController.repeat();
-          }
-        },
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Text(
+              equation,
+              style: TextStyle(fontSize: equationFontSize, color: Colors.white),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Text(
+              result,
+              style: TextStyle(fontSize: resultFontSize, color: Colors.white),
+            ),
+          ),
+          Expanded(child: Divider()),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: screenSize.width * 0.75,
+                child: Table(
+                  children: [
+                    TableRow(children: [
+                      buidButton('C', 1, Color(0xff4E505F)),
+                      buidButton('←', 1, Color(0xff4E505F)),
+                      buidButton('÷', 1, Color(0xff4E505F))
+                    ]),
+                    TableRow(children: [
+                      buidButton('7', 1, Color(0xff2E2F38)),
+                      buidButton('8', 1, Color(0xff2E2F38)),
+                      buidButton('9', 1, Color(0xff2E2F38))
+                    ]),
+                    TableRow(children: [
+                      buidButton('4', 1, Color(0xff2E2F38)),
+                      buidButton('5', 1, Color(0xff2E2F38)),
+                      buidButton('6', 1, Color(0xff2E2F38))
+                    ]),
+                    TableRow(children: [
+                      buidButton('1', 1, Color(0xff2E2F38)),
+                      buidButton('2', 1, Color(0xff2E2F38)),
+                      buidButton('3', 1, Color(0xff2E2F38))
+                    ]),
+                    TableRow(children: [
+                      buidButton('.', 1, Color(0xff2E2F38)),
+                      buidButton('0', 1, Color(0xff2E2F38)),
+                      buidButton('00', 1, Color(0xff2E2F38))
+                    ]),
+                  ],
+                ),
+              ),
+              Container(
+                width: screenSize.width * 0.25,
+                child: Table(
+                  children: [
+                    TableRow(children: [buidButton('×', 1, Color(0xff4B5EFC))]),
+                    TableRow(children: [buidButton('-', 1, Color(0xff4B5EFC))]),
+                    TableRow(children: [buidButton('+', 1, Color(0xff4B5EFC))]),
+                    TableRow(
+                        children: [buidButton('=', 2.2, Color(0xff4B5EFC))]),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
